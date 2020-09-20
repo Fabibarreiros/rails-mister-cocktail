9149c82122de22ce4c16ed5696d081ffca6016b3
Fabiana Silva <fabibarreiros@gmail.com>
2020-09-20 20:50:21 -0300

    com reviews

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,37 +1,29 @@
 class DosesController < ApplicationController
-
-  before_action :set_dose, only: [:destroy]
-
   def new
-    @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
-  # POST /restaurants
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to @dose.cocktail, notice: 'dose was successfully created.'
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render 'cocktails/show'
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to doses_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_dose
-    @dose = Dose.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
